31514bc3fb7016150633eec49dfafc93f6f6bd05
cyhFlight <yuhangc@della5.princeton.edu>
2020-01-22 22:53:02 -0500

    edit SBATCH parameters

--- yc_runBox.sh
+++ yc_runBox.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+#SBATCH --job-name=videoMATLAB
 #SBATCH -N 1
 #SBATCH --time=2:59:00
 #SBATCH --mem=150000
 #SBATCH -c 16
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=yuhangc@princeton.edu
+#SBATCH -o /projects/WANG/mouseBehavior/slurm_logs/slurm-%A_%a.out # STDOUT
+#SBATCH -D /projects/WANG/mouseBehavior/
 #SBATCH --array=1-10
 
 export MATLABPATH='/tigress/WANG/mouseBehavior/leap/'
@@ -13,8 +19,6 @@ cd $MATLABPATH
 #source activate automice 
 
 listOfFiles="/tigress/yuhangc/wang_lab/test_run/TEST_List.txt"
-
-
 rowNumber=$SLURM_ARRAY_TASK_ID
 fileName=$(sed "${rowNumber}q;d" $listOfFiles)
 /usr/licensed/bin/matlab -nosplash -nodesktop -nodisplay -singleCompThread -nojvm -r "makeMouseBox3('$fileName','/tigress/yuhangc/wang_lab/test_run/box'); exit;"
